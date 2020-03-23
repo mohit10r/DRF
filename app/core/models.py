a1bf41cd65829c333c7e4ba0db22905cbea45435
Mohit <mohitranjan28@gmail.com>
2020-03-23 10:07:14 +0530

    core app created

--- app/core/models.py
+++ app/core/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                                         PermissionsMixin
 
 
 class UserManager(BaseUserManager):
 
-	def create_user(self, email, password=None, **extra_fields):
-		"""Creates and saves the new user"""
-		user = self.model(email=email, **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves the new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-		return user
-
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
